01772ff99d32078caf350ef84d98abeb419196f5
asataka <e70502@gmail.com>
2022-03-19 00:46:28 +0900

    Add Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const App = () => {
-  return (
-    <div className="App" />
-  );
-}
+  return <div className="App" />;
+};
 
 export default App;
